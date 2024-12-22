c14f23a889084a4a92a4bee91ee0d991ff1d6afe
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-12-22 17:34:11 +0900

    Bump `MachOKit` to 0.26.2

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/p-x9/MachOKit.git", from: "0.26.1"),
+        .package(url: "https://github.com/p-x9/MachOKit.git", from: "0.26.2"),
         .package(url: "https://github.com/p-x9/swift-objc-dump.git", from: "0.6.0")
     ],
     targets: [
